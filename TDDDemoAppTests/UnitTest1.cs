2f4723a741e650440443c81d840e0bf5eecd9012
BartusZak <b.ploszynski@gmail.com>
2018-03-17 13:54:00 +0100

    Write test.

--- TDDDemoAppTests/UnitTest1.cs
+++ TDDDemoAppTests/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace TDDDemoAppTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Fact]
+        public void GetTotalWordCount_WhenPassedRegularText_ShouldReturnExpectedCount()
+        {
+            //arrange
+            const string textToAnalyze = @"xUnit.net is a free, open source, community-focused 
+                                            unit testing tool for the .NET Framework.";
+
+            var analyzer = new TextAnalyzer();
+
+            //act
+            var count = analyzer.GetTotalWordCount(textToAnalyze);
+
+            //assert
+            Assert.Equal(14, count);
         }
     }
 }
