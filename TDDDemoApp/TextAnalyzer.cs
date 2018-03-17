0521b8bb3fe68c0fd87b162f272f54a919b1673f
BartusZak <b.ploszynski@gmail.com>
2018-03-17 14:00:02 +0100

    Adding reference and class.

---
+++ TDDDemoApp/TextAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tdddemoapp
+{
+    public class TextAnalyzer
+    {
+
+        public int GetTotalWordCount(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- TDDDemoAppTests/UnitTest1.cs
+++ TDDDemoAppTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using tdddemoapp;
 using Xunit;
 
 namespace TDDDemoAppTests
